abe29f1f1960f6dcb8b7ceeaece0faf4c1d519b2
SafeZone-Ucam <jonathan.dev_93@outlook.com>
2024-06-28 17:44:06 -0300

    Criado um componente teste HelloSafe

---
+++ src/components/HelloSafe.js
@@ -0,0 +1,16 @@
+import MyButton from "./MyButton"
+
+const servidor = "Webpack rodando com sucesso!"
+function HelloSafe(){
+    return(
+        <div>
+            <h1> Seja bem-vindo ao Safezone</h1>
+            <p>Clique nesse botão para logar e navegar</p>
+            <MyButton></MyButton>
+            
+            <p>{servidor}</p>
+
+        </div>    
+    )
+}
+export default HelloSafe
---
+++ src/components/MyButton.js
@@ -0,0 +1,8 @@
+function MyButton(){
+    return(
+        <button> Clique Aqui</button>
+
+    )
+}
+
+export default MyButton
