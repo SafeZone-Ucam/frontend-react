bfe79743a16076069bcd3b4280331c2f201fe15e
SafeZone-Ucam <ucam.safezone@gmail.com>
2024-07-08 19:45:31 -0300

    arnMerge branch 'main' of https://github.com/SafeZone-Ucam/safezone-frontend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import HelloSafe from './components/HelloSafe';
-import logo from './logo.svg';
-import './App.css'
 
 function App() {
   return (
@@ -9,20 +7,7 @@ function App() {
 
       <h1> Safezone</h1>
       <HelloSafe/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
 </div>
   );
 }
